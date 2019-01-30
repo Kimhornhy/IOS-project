904904d0e22e62356ed15eec861e9d2bd6cbf981
Kimhorn <Kimhorn41@gmail.com>
2019-01-30 11:52:40 +0700

    add model

---
+++ ContactAppCoreDataDemo/model.swift
@@ -0,0 +1,16 @@
+//
+//  model.swift
+//  ContactAppCoreDataDemo
+//
+//  Created by MAC on 1/30/19.
+//  Copyright © 2019 MAC. All rights reserved.
+//
+
+import Foundation
+class a {
+    func  p() {
+        
+    }
+}
+
+
